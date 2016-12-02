fefbca32e1b4e359d2d2ade2465dd0081751f2ef
Efren De la mora <efrendelamora@la-mbp-de-efren.lan>
2016-12-02 09:28:53 -0600

    Initial Commit

---
+++ mobileAdminProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mobileAdminProject
+//
+//  Created by Efren De la mora on 02/12/16.
+//  Copyright © 2016 Efren De la mora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
